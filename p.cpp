c3ecd91d934e8c5f4d0ebf0f144c49e60eb9b6b5
JyotiPrakashBhanja <jp323399@gmail.com>
2026-04-20 12:21:34 +0530

    first commit

---
+++ p.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main() {
+//     int n;
+//     cout << "Enter :";
+//     cin >> n;
+// for (int i= 1; i <=n; i++){
+//     for (int j=1; j<=i; j++){
+//         cout<<i<<" ";
+//     }
+//     cout << endl;
+// }
+
+
+int n;
+int num = 1;
+cout << "Enter :";
+cin>>n;
+
+for (int i =1;i <=n; i++ ){
+    for(int j =1; j<=i; j++){
+        cout<<num<< " ";
+        num++;
+
+    }
+    cout<<endl;
+}
+
+
+
+    return 0;
+}
+
+
